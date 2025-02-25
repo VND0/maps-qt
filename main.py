2b6dcdf69e1660094601e4e9ae87aa92c925036c
nikita <vnd-08@yandex.ru>
2025-02-25 19:35:38 +0300

    Added scaling with PgUp and PgDown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtGui import QPixmap, QShortcut, QKeySequence
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
 from desisn import Ui_MainWindow
@@ -12,6 +12,13 @@ class MapApp(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.init_app()
+        self.add_shortcuts()
+
+    def set_map_image(self):
+        elements = get_image(self.api_key, to_api_ll(*self.ll), to_api_spn(self.spn))
+        map_pixmap = QPixmap()
+        map_pixmap.loadFromData(elements, "PNG")
+        self.map_lbl.setPixmap(map_pixmap)
 
     def init_app(self):
         self.ll = 61.668797, 50.836497
@@ -19,12 +26,27 @@ class MapApp(QMainWindow, Ui_MainWindow):
         self.api_key = "d94029a4-b4b6-48db-b4b3-cc9085862f55"
 
         try:
-            elements = get_image(self.api_key, to_api_ll(*self.ll), to_api_spn(self.spn))
-            map_pixmap = QPixmap()
-            map_pixmap.loadFromData(elements, "PNG")
-            self.map_lbl.setPixmap(map_pixmap)
+            self.set_map_image()
         except ApiException:
             self.statusbar.showMessage(str(ApiException))
+
+    def add_shortcuts(self):
+        self.pg_up = QShortcut(QKeySequence("PgUp"), self)
+        self.pg_up.activated.connect(lambda: self.change_spn(1))
+        self.pg_down = QShortcut(QKeySequence("PgDown"), self)
+        self.pg_down.activated.connect(lambda: self.change_spn(-1))
+
+    def change_spn(self, how: int):
+        backup = self.spn
+        self.spn += self.spn * 0.5 * how
+
+        try:
+            assert self.spn > 0.0002  # Потом что-то происходит странное с картами
+            self.set_map_image()
+            self.statusbar.clearMessage()
+        except (ApiException, AssertionError):
+            self.statusbar.showMessage("Достигнуто предельное значение масштаба")
+            self.spn = backup
 
 
 def except_hook(cls, exception, traceback):
